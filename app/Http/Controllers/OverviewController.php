1ed59e33ecdb1321f17905db08d24fd0cd2daa02
freakpants <freakpants@gmail.com>
2019-02-26 21:09:00 +0100

    set up login etc

--- app/Http/Controllers/OverviewController.php
+++ app/Http/Controllers/OverviewController.php
@@ -1,10 +1,9 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class OverviewController extends Controller
 {
-    //
+	public function show(Request $request){
+		return view('welcome');
+	}
 }
